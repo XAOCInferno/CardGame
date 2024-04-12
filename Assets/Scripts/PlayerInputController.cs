5b1cb56482fdfa8b7b46e29bc8b468ea6f98455d
XAOCInferno <TestXBot@gmail.com>
2024-04-12 13:59:29 +0100

    Remove redundant imports

--- Assets/Scripts/PlayerInputController.cs
+++ Assets/Scripts/PlayerInputController.cs
@@ -1,9 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.UI;
 
 [RequireComponent(typeof(PlayerInput))]
 public class PlayerInputController : MonoBehaviour
